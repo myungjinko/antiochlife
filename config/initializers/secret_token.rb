ab09871d2cdc2f2d9ea55f4833e8dc0a94cd1a20
myungjinko <myungjinko@hotmail.com>
2013-11-27 13:39:59 -0800

    secret_token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Antiochlife::Application.config.secret_key_base = 'a50d9975f029c8ea4bc23eb8bf0e5cc3fd8dc3bd88eca0e49b22d9a56d72431ee15fe88e8c52f85eebe89db35c032f35df92dfc3863b11007e93883953573762'
