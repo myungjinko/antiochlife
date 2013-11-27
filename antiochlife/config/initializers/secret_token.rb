b75c89b96a3ad6349c630800c1313a43e7e0a17e
myungjinko <myungjinko@hotmail.com>
2013-11-27 12:30:31 -0800

    Initial Setup

---
+++ antiochlife/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Antiochlife::Application.config.secret_key_base = '4fcdef3143045bc8be07708a854f02b1ec21929988ad3c6aede6c8d75114829d5924defd5297c8ebab4f73fe3a38f366288528a25acd08b1bf7689de9b818752'
